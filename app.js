89e91e902addf5882a747bf8e40f033b87556672
Marcia Branquinho Garcia <mabraquinho.garcia@gmail.com>
2025-08-19 23:53:38 -0300

    Funcionalidades de adicionar, listar e sortear implementadas

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById('amigo').value;
+
+    if (nomeAmigo == '') {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+    console.log(amigos);
+
+    document.getElementById('amigo').value = '';
+
+    atualizarLista();
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML = lista.innerHTML + '<li>' + amigos[i] + '</li>';
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        alert('Adicione pelo menos um amigo para realizar o sorteio!');
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = '<p>O amigo secreto sorteado é: ' + amigoSorteado + '</p>';
+}
